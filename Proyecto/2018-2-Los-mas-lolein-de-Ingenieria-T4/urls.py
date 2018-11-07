38e2a0d08554a23cbf3136912181124411c6cd37
mauro <mauricio.araneda@ug.uchile.cl>
2018-11-07 18:50:17 -0300

    carpetas ordenadas

--- Proyecto/2018-2-Los-mas-lolein-de-Ingenieria-T4/urls.py
+++ Proyecto/2018-2-Los-mas-lolein-de-Ingenieria-T4/urls.py
@@ -1,4 +1,4 @@
-"""untitled URL Configuration
+"""2018-2-Los-mas-lolein-de-Ingenieria-T4 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
